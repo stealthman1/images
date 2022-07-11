709c76687a8fcffee590ede7650968605cd604e3
stealthman1 <timur.marufiy@gmail.com>
2022-07-11 18:04:40 +0300

    display and del images

---
+++ frontend/src/components/ImageCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const ImageCard = ({ image, deleteImage }) => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image.urls.small} />
+      <Card.Body>
+        <Card.Title>{image.title.toUpperCase()}</Card.Title>
+        <Card.Text>{image.description || image.alt.description}</Card.Text>
+        <Button variant="primary" onClick={() => deleteImage(image.id)}>
+          Delete
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ImageCard;
