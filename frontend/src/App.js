4e44a4b252491549a035d22df4647d8023c88907
stealthman1 <timur.marufiy@gmail.com>
2022-07-11 12:20:17 +0300

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-
+const UNSPLASH_KEY= process.env.REACT_APP_UNSPLASH_KEY
 
 
 const App = () => {
@@ -13,7 +13,16 @@ const App = () => {
   const handleSearchSubmit = (e) => {
     e.preventDefault(); 
     console.log(word);
+    fetch(`https://api.unsplash.com//photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
+
 
   return (
     <div className="App">
