9245b07c21d4f85d0f281236e8769b1959336ca0
stealthman1 <timur.marufiy@gmail.com>
2022-07-12 17:13:20 +0300

    welcome component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,7 +11,7 @@ const Header = (props) => {
   return (
     <Navbar style={navBarStyle} variant="light">
       <Container>
-        <Logo style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
+        <Logo alt={title} style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
       </Container>
     </Navbar>
   );
